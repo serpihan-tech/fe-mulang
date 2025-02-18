b7288b2649548f7ae617b21a1b22691336411fa9
Melinda Wijaya <gmy4784@gmail.com>
2025-02-18 12:46:27 +0700

    style\: LoginForm

--- app/component/LoginForm.js
+++ app/component/LoginForm.js
@@ -1,16 +1,16 @@
 export default function LoginForm() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <div className="bg-white p-8 rounded-2xl shadow-2xl flex w-[800px]">
+      <div className="bg-white px-16 py-20 rounded-2xl shadow-[8px_4px_64px_rgba(0,0,0,0.25)] flex w-[1000px]">
         <div className="w-1/2 flex items-center justify-center">
-          <img src="/login.png" alt="Login" className="object-contain h-60 w-full" />
+          <img src="/login.svg" alt="Login" className="object-contain h-80 w-full" />
         </div>
 
         <div className="w-1/2 px-6 flex flex-col justify-center items-center">
-          <h2 className="text-2xl font-bold text-gray-900 mb-2">Selamat Datang!</h2>
-          <p className="text-gray-600 mb-6">Masuk ke akun anda</p>
-          <form className="space-y-4">
-            <div>
+          <h2 className="text-4xl font-semibold text-blue-600 mb-2">Selamat Datang!</h2>
+          <p className="text-xl text-gray-600 mb-[40px] font-light">Masuk ke akun anda</p>
+          <form className="w-full">
+            <div className="w-full mb-4">
               <label className="block text-sm font-medium text-gray-700">Email</label>
               <input
                 type="email"
@@ -18,7 +18,7 @@ export default function LoginForm() {
                 required
               />
             </div>
-            <div>
+            <div className="w-full mb-[40px]">
               <label className="block text-sm font-medium text-gray-700">Kata Sandi</label>
               <input
                 type="password"
@@ -26,13 +26,14 @@ export default function LoginForm() {
                 required
               />
             </div>
-
-            <button
-              type="submit"
-              className="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition"
-            >
-              Masuk
-            </button>
+            <div className="flex justify-center">
+              <button
+                type="submit"
+                className="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition"
+              >
+                Masuk
+              </button>
+            </div>
           </form>
         </div>
       </div>
--- app/login/page.js
+++ app/login/page.js
@@ -1,9 +1,22 @@
 import LoginForm from "../component/LoginForm";
+
 export default function LoginPage() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="relative min-h-screen overflow-hidden flex items-center justify-center">
+      <img 
+        src="/ellipse_top.svg" 
+        alt="Background" 
+        className="absolute -top-16 -left-10 h-3/4 w-1/2 -z-10" 
+      />
+      
       <LoginForm/>
+
+      <img 
+        src="/ellipse_bottom.svg" 
+        alt="Background" 
+        className="absolute -bottom-80 -right-24 h-3/4 w-1/2 -z-10" 
+      />
+
     </div>
   );
 }
-
