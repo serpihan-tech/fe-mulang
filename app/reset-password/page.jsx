7b7f3c0c8b39b16cf79d0bf658de6081886a6b71
Melinda Wijaya <gmy4784@gmail.com>
2025-02-18 14:38:11 +0700

    feat: add ResetPassword

---
+++ app/reset-password/page.jsx
@@ -0,0 +1,9 @@
+import ResetPassword from "../component/ResetPassword.jsx";
+
+export default function ResetPasswordPage() {
+  return (
+    <div className="relative bg-[#CED9F9] min-h-screen overflow-hidden flex items-center justify-center">
+      <ResetPassword/>
+    </div>
+  );
+}
