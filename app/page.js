f6ecca6e72e340a36b990befc20adde1ed1e5fd2
Melinda Wijaya <gmy4784@gmail.com>
2025-02-19 15:11:51 +0700

    style: fix home bg color

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid bg-white items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 items-center sm:items-start">
         <div className="w-full flex items-center justify-center">
           <Image
