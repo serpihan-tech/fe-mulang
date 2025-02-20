2f2a39b1c03c92b45587b6142751f1c2be5427dd
Melinda Wijaya <gmy4784@gmail.com>
2025-02-20 15:15:02 +0700

    feature: top card

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,9 +1,38 @@
+import { CalendarDays } from "lucide-react";
+import Card from "./_component/Card";
+import { Users } from "lucide-react";
 export default function Dashboard() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-5xl font-extrabold text-blue-900 tracking-widest ms-4">
-        Dashboard
-      </h1>
+    <div className="flex bg-[#FAFAFA] pt-8 px-6 gap-5">
+      <Card>
+        <Users className="w-9 h-9 bg-blue-600 p-1.5 rounded-md text-white text-extrabold"/>
+        <p className="text-lg font-bold py-4">1025</p>
+        <p className="text-sm font-medium" >Total Siswa</p>
+      </Card>
+      <Card>
+        <Users className="w-9 h-9 bg-blue-600 p-1.5 rounded-md text-white text-extrabold"/>
+        <p className="text-lg font-bold py-4">100</p>
+        <p className="text-sm font-medium" >Total Guru</p>
+      </Card>
+      <Card>
+        <Users className="w-9 h-9 bg-blue-600 p-1.5 rounded-md text-white text-extrabold"/>
+        <p className="text-lg font-bold py-4">150</p>
+        <p className="text-sm font-medium" >Total Mapel</p>
+      </Card>
+      <Card>
+        <Users className="w-9 h-9 bg-blue-600 p-1.5 rounded-md text-white text-extrabold"/>
+        <p className="text-lg font-bold py-4">25000</p>
+        <p className="text-sm font-medium" >Total Kelulusan</p>
+      </Card>
+      <div className="w-[332px] h-16 flex p-3.5 rounded-md bg-white gap-2">
+        <div className="flex items-center">
+          <CalendarDays className="flex items-center justify-center w-7.5 rounded-md text-gray-700 text-extrabold "/>
+        </div>
+        <div>
+          <p className="text-base font-bold">Periode</p>
+          <p className="text-[10px] font-medium" >Genap 2024-2025</p>
+        </div>
+      </div>
     </div>
   );
 }
