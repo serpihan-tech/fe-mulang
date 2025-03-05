59b8af4bcb287d81c9d96c78ada4b992fdf8fda1
Melinda Wijaya <gmy4784@gmail.com>
2025-03-05 15:10:28 +0700

    feat: add succes and failed upload file pop up

--- app/component/SuccessUploadPopUp.jsx
+++ app/component/SuccessUploadPopUp.jsx
@@ -1,21 +1,20 @@
 "use client"
-import { CloseCircle } from "iconsax-react";
 import Image from "next/image";
 
-export default function SuccessPopUp() {
+export default function SuccessUploadPopUp() {
   return (
     <div className="w-[516.27px] bg-white rounded-2xl p-[32.4px]">
       <div className="w-full flex justify-center">
         <Image
-          src="/svg/success.svg"
-          alt="success"
+          src="/svg/upload.svg"
+          alt="upload"
           width={304}
           height={211}
         />
       </div>
       <div className="w-full flex flex-col items-center justify-center mt-[24.3px] space-y-[10.12px]">
-        <p className="text-xl font-medium">Pembaruan Berhasil</p>
-        <p className="text-lg font-normal">Pembaruan yang anda buat telah tersimpan</p>
+        <p className="text-xl font-medium">File berhasil diunggah</p>
+        <p className="text-lg font-normal">File yang anda unggah telah tersimpan</p>
       </div>
     </div>
   )
