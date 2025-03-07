f70893bccacba3440f1de5986a92636773b93251
Melinda Wijaya <gmy4784@gmail.com>
2025-03-07 11:06:32 +0700

    style: fix MapelCard style

--- app/kbm/mata-pelajaran/pages/Teacher.jsx
+++ app/kbm/mata-pelajaran/pages/Teacher.jsx
@@ -10,8 +10,9 @@ export default function MataPelajaranTeacher() {
         <div>
           <div className="w-full ps-2 mt-12 space-y-8">
             <h1 className="w-full text-black text-xl font-semibold">Mata Pelajaran</h1> 
-            <div className="flex space-x-7 space-y-6"></div>
+            <div className="flex gap-6 max-w-full flex-wrap px-5">
               <MapelCard
+                path={"/svg/physic.svg" }
                 bgColor={"bg-[#0841e2]"}
                 title={"Fisika"}
                 kelas={"X MIPA 1"}
@@ -19,6 +20,7 @@ export default function MataPelajaranTeacher() {
                 totalStudents={34}
               />
               <MapelCard
+                path={"/svg/physic.svg" }
                 bgColor={"bg-[#0841e2]"}
                 title={"Fisika"}
                 kelas={"XI MIPA 1"}
@@ -26,12 +28,44 @@ export default function MataPelajaranTeacher() {
                 totalStudents={34}
               />
               <MapelCard
+                path={"/svg/physic.svg" }
                 bgColor={"bg-[#0841e2]"}
                 title={"Fisika"}
                 kelas={"XII MIPA 1"}
                 icon={Profile2User}
                 totalStudents={34}
               />
+              <MapelCard
+                path={"/svg/math.svg" }
+                bgColor={"bg-[#FFCF43]"}
+                title={"Matematika"}
+                kelas={"X MIPA 1"}
+                icon={Profile2User}
+                totalStudents={34}
+                textColor={"black"}
+                colorIcon={"black"}
+              />
+              <MapelCard
+                path={"/svg/math.svg" }
+                bgColor={"bg-[#FFCF43]"}
+                title={"Matematika"}
+                kelas={"XI MIPA 1"}
+                icon={Profile2User}
+                totalStudents={34}
+                textColor={"black"}
+                colorIcon={"black"}
+              />
+              <MapelCard
+                path={"/svg/math.svg" }
+                bgColor={"bg-[#FFCF43]"}
+                title={"Matematika"}
+                kelas={"XII MIPA 1"}
+                icon={Profile2User}
+                totalStudents={34}
+                textColor={"black"}
+                colorIcon={"black"}
+              />
+            </div>
           </div>
         </div>
       </div>  
