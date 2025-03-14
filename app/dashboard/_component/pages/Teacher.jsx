ff5dd89ea0acdcaafeefcff359a8e28182bdbfba
Melinda Wijaya <gmy4784@gmail.com>
2025-03-14 09:57:42 +0700

    feat: add Card2 to dashboard

--- app/dashboard/_component/pages/Teacher.jsx
+++ app/dashboard/_component/pages/Teacher.jsx
@@ -5,6 +5,11 @@ import Periode from "../home/Periode";
 import HomeCalendar from "../home/HomeCalendar";
 import JadwalMengajar from "../home/JadwalMengajar";
 import Informasi from "../home/Informasi";
+<<<<<<< Updated upstream
+=======
+import Card2 from "../home/Card2";
+import { Book, People } from "iconsax-react";
+>>>>>>> Stashed changes
 
 export default function TeacherDashboard() {
   return (
@@ -14,6 +19,7 @@ export default function TeacherDashboard() {
       
         <div className="bg-[#FAFAFA]  dark:bg-black flex pt-8 px-6 gap-5 ">
           
+<<<<<<< Updated upstream
           <div className="px-8 space-y-5">
             <div className="flex gap-5">
               <Card2 
@@ -29,6 +35,25 @@ export default function TeacherDashboard() {
                 label="Siswa"
                 bgColor={"bg-[#fff5d9]"}
                 colorBehind={"bg-[#FFCF43]"}
+=======
+          <div className="px-4">
+            <div className="w-full flex gap-6">
+              <Card2
+                bgColor={"bg-indigo-200"}
+                colorBehind={"bg-[#0841e2]"}
+                icon={Book}
+                value={"6"}
+                label={"Kelas"}
+                className="w-1/2"
+              />
+              <Card2
+                bgColor={"bg-orange-100"}
+                colorBehind={"bg-amber-300"}
+                icon={People}
+                value={"160"}
+                label={"Siswa"}
+                className="w-1/2"
+>>>>>>> Stashed changes
               />
             </div>
 
