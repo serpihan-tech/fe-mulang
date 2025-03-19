d2a64887b6bcb90f57d5a2e4932721eb9eb0e6ec
RizalAnas00 <belangsumerlang@gmail.com>
2025-03-19 13:50:51 +0700

    add: uid generator

--- lib/transmit.jsx
+++ lib/transmit.jsx
@@ -5,4 +5,14 @@ import { Transmit } from '@adonisjs/transmit-client'
 export const transmit = new Transmit({
   baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
   withCredentials: true,
+  uidGenerator: () => {
+    return "123456"
+  },
+  beforeSubscribe: (channel) => {
+    return {
+      headers: {
+        Authorization: `Bearer ${sessionStorage.getItem('token')}`
+      }
+    }
+  }
 })
