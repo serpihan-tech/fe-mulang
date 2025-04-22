705478ef9d21dc15fd17d3302b579e4078c73dd0
Melinda Wijaya <gmy4784@gmail.com>
2025-04-22 10:19:04 +0700

    style: fix mata pelajaran responsive for tablet and handphone

--- app/(dashboard)/dashboard/_component/home/Presensi.jsx
+++ app/(dashboard)/dashboard/_component/home/Presensi.jsx
@@ -6,9 +6,9 @@ export default function Presensi () {
           <h1 className="text-black text-sm md:text-base lg:text-lg font-semibold">Presensi Harian</h1>
           <p className="text-[#7f7f7f] text-[10px] md:text-xs lg:text-base font-normal">Senin, 2 Februari 2025</p>
         </div>
-        <div className="px-3 py-3 md:px-8 md:py-3 bg-[#0841e2] rounded-[5px]">
+        <button className="px-3 py-3 md:px-8 md:py-3 bg-[#0841e2] rounded-[5px]">
           <div className="text-white text-[10px] md:text-xs lg:text-sm font-semibold">Presensi Masuk</div>
-        </div>
+        </button>
       </div>
       <div className="w-full flex space-x-3 md:space-x-5">
         <div className="w-1/2 py-2 px-4 space-y-1 lg:space-y-2 bg-[#cee8d6] rounded-[10px] flex-col justify-start items-start inline-flex">
