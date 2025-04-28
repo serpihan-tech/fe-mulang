1af0f514510e1b44fa5b4656b8baf9d9b58ca3dc
Melinda Wijaya <gmy4784@gmail.com>
2025-04-28 11:36:25 +0700

    style: fix dark mode for notification dropdown

--- app/(dashboard)/notification/_component/Notif.jsx
+++ app/(dashboard)/notification/_component/Notif.jsx
@@ -36,7 +36,7 @@ export default function Notif({
   };
 
   return (
-    <div className="w-full flex self-stretch p-3 rounded-[10px] outline outline-[0.50px] outline-offset-[-0.50px] outline-neutral-400 gap-2 items-center">
+    <div className="w-full flex self-stretch p-3 rounded-[10px] bg-white dark:bg-dark_net-ter outline outline-[0.50px] outline-offset-[-0.50px] outline-neutral-400 gap-2 items-center">
       <div className={`${hidden} w-11 h-11 lg:w-14 lg:h-14 rounded-full border border-stone-300 items-center`}>
         <Image 
           src={imgSource} 
@@ -49,21 +49,21 @@ export default function Notif({
       <div className={`w-full ${contentGap}`}>
         <div className="w-full flex justify-between items-center">
           <div className="flex items-center"> 
-            <h2 className="justify-center text-blue-700 text-sm md:text-base lg:text-lg font-bold">
+            <h2 className="justify-center text-blue-700 dark:text-[#5D8BF8] text-sm md:text-base lg:text-lg font-bold">
               {sender}
             </h2>
             {renderAdditionalInfo()}
           </div>
           <div className="flex gap-1">
-            <div className="text-black text-xs font-normal">{date}</div>
-            <div className="w-1.5 h-1.5 bg-red-600 rounded-full" />
+            <div className="text-black dark:text-slate-100 text-xs font-normal">{date}</div>
+            <div className="w-1.5 h-1.5 bg-red-600 dark:bg-[#ff4022] rounded-full" />
           </div>
         </div>
         <div className="flex space-x-2 items-start">
-          <div className="text-black text-xs md:text-sm lg:text-base font-bold whitespace-nowrap">
+          <div className="text-black dark:text-slate-100 text-xs md:text-sm lg:text-base font-bold whitespace-nowrap">
             {title}
           </div>
-          <p className="text-neutral-600 text-xs md:text-sm lg:text-base font-normal 
+          <p className="text-neutral-600 dark:text-slate-300 text-xs md:text-sm lg:text-base font-normal 
             line-clamp-1 
             overflow-hidden 
             text-ellipsis">
