b1390e9810b45431f0d1af24dc0bab478f315b5b
Melinda Wijaya <gmy4784@gmail.com>
2025-05-02 09:56:02 +0700

    style: fix tambah ruangan modal dark mode

--- app/(dashboard)/kepegawaian/_component/DataRuanganModal.jsx
+++ app/(dashboard)/kepegawaian/_component/DataRuanganModal.jsx
@@ -29,8 +29,8 @@ export default function DataRuanganModal({ onCancel, onConfirm, ruanganData, isL
   console.log("ruanganData", ruanganData)
 
   return (
-    <div className="z-30 fixed inset-0 bg-black/50 flex justify-center items-center text-black">
-      <div className="bg-white w-full max-w-md rounded-xl p-6 shadow-lg">
+    <div className="z-30 fixed inset-0 bg-black/50 flex justify-center items-center text-black dark:text-slate-100">
+      <div className="bg-white dark:bg-dark_net-pri w-full max-w-md rounded-xl p-6 shadow-lg">
 
         <div className="flex justify-between items-center mb-4">
             <h2 className="text-lg font-semibold">{ ruanganData ? "Edit" : "Tambah" } Data</h2>
@@ -60,16 +60,16 @@ export default function DataRuanganModal({ onCancel, onConfirm, ruanganData, isL
         <div className="flex justify-end mt-6 space-x-3">
           <button
             onClick={onCancel}
-            className="px-4 py-2 border border-red-500 text-red-500 rounded-md hover:bg-red-50"
+            className="px-4 py-2 text-sm font-medium border rounded-md border-red-600 dark:border-[#ff4022] text-red-600 dark:text-[#ff4022] hover:bg-red-500 dark:hover:bg-[#ff4022] dark:hover:text-slate-100 hover:text-white bg-white dark:bg-dark_net-quar transition-shadow duration-300 hover:scale-105"
           >
             Batal
           </button>
           <button
             onClick={handleSubmit}
             disabled={isLoading}
-            className={`px-4 py-2 rounded-md text-white ${
+            className={`px-4 py-2 text-sm font-medium rounded-md text-white duration-300 hover:scale-105 ${
                 isLoading
-                ? "bg-blue-300 cursor-not-allowed"
+                ? "bg-blue-300 cursor-not-allowed dark:text-black"
                 : "bg-blue-600 hover:bg-blue-700"
             }`}
             >
@@ -90,7 +90,7 @@ const Input = ({ label, value, disabled, onChange, placeholder="-" }) => (
             onChange={onChange}
             disabled={disabled}
             placeholder={placeholder}
-            className={`w-full mt-1 text-sm  text-black px-3 py-2 border border-gray-300 rounded-md ${disabled ? "bg-gray-200 text-black" : "bg-white"}`}
+            className={`w-full mt-1 text-sm  text-black px-3 py-2 border border-gray-300 rounded-md ${disabled ? "bg-gray-200 text-black dark:bg-dark_net-ter dark:text-slate-100" : "bg-white dark:bg-dark_net-ter dark:text-slate-100"}`}
         />
     </div>
 );
