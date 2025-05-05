c68afa6e3775cd5c1e84b81ff1592be133e5dd8a
Melinda Wijaya <gmy4784@gmail.com>
2025-05-05 11:12:43 +0700

    style: fix jadwal pelajaran admin dark mode

--- app/(dashboard)/kbm/jadwal-pelajaran/page.jsx
+++ app/(dashboard)/kbm/jadwal-pelajaran/page.jsx
@@ -316,7 +316,7 @@ export default function JadwalPelajaran() {
       <div className="z-0 transition">
         <div>
           <div className="w-full ps-2 flex">
-            <h1 className="w-full text-black text-xl font-semibold">Jadwal Pelajaran</h1> 
+            <h1 className="w-full text-black dark:text-white text-xl font-semibold">Jadwal Pelajaran</h1> 
             <div className="w-full flex items-center justify-end gap-5">
               <SmallButton
                 type="button"
@@ -331,7 +331,7 @@ export default function JadwalPelajaran() {
           </div>
 
           <div className="flex flex-col justify-end bg-white dark:bg-dark_net-pri rounded-lg my-5">
-                <div className={jadwalData ? "max-w-full p-5" : "flex items-center justify-center text-black dark:text-white p-28"}>
+                <div className={jadwalData ? "max-w-full p-5 dark:bg-dark_net-ter" : "flex items-center justify-center text-black dark:text-white p-28"}>
                     {jadwalData ? 
                       <TableComponent 
                           dataKey='id_jadwal'
